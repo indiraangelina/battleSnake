5193c6ef351c14e83bf48e35aa4921df18694be8
Indira <Indira.Schnatmann@arvato-scs.com>
2022-08-26 08:30:50 +0200

    find food

--- src/logic.ts
+++ src/logic.ts
@@ -46,88 +46,81 @@ export function move(gameState: GameState): MoveResponse {
     } else if (myNeck.y > myHead.y) {
         possibleMoves.up = false
     }
-    
+
     // TODO: Step 1 - Don't hit walls.
     // Use information in gameState to prevent your Battlesnake from moving beyond the boundaries of the board.
 
-    if (myHead.y === (maxheight -1) ) {
+    if (myHead.y === (maxheight - 1)) {
         possibleMoves.up = false
-    } 
-
-    if (myHead.y === minheigth ) {
-        possibleMoves.down = false 
     }
 
-    if (myHead.x === minwidth ) {
-        possibleMoves.left = false 
+    if (myHead.y === minheigth) {
+        possibleMoves.down = false
     }
 
-    if (myHead.x === (maxwidth -1)) {
-        possibleMoves.right = false 
+    if (myHead.x === minwidth) {
+        possibleMoves.left = false
     }
-        
+
+    if (myHead.x === (maxwidth - 1)) {
+        possibleMoves.right = false
+    }
+
     // TODO: Step 2 - Don't hit yourself.
     // Use information in gameState to prevent your Battlesnake from colliding with itself.
     const myBody = gameState.you.body
-    const targetUp: Coord = {x: myHead.x , y: myHead.y +1 }
-    const targetDown: Coord = {x: myHead.x , y: myHead.y -1}
-    const targetLeft: Coord = {x: myHead.x -1 , y: myHead.y}
-    const targetRight: Coord = {x: myHead.x +1 , y: myHead.y}
+    const targetUp: Coord = { x: myHead.x, y: myHead.y + 1 }
+    const targetDown: Coord = { x: myHead.x, y: myHead.y - 1 }
+    const targetLeft: Coord = { x: myHead.x - 1, y: myHead.y }
+    const targetRight: Coord = { x: myHead.x + 1, y: myHead.y }
 
     if (myBody.some(coord => coord.x === targetUp.x && coord.y === targetUp.y)) {
         possibleMoves.up = false
     }
     if (myBody.some(coord => coord.x === targetDown.x && coord.y === targetDown.y)) {
-        possibleMoves.down = false 
+        possibleMoves.down = false
     }
     if (myBody.some(coord => coord.x === targetLeft.x && coord.y === targetLeft.y)) {
-        possibleMoves.left = false 
+        possibleMoves.left = false
     }
     if (myBody.some(coord => coord.x === targetRight.x && coord.y === targetRight.y)) {
-        possibleMoves.right = false 
+        possibleMoves.right = false
     }
 
     // TODO: Step 3 - Don't collide with others.
     // Use information in gameState to prevent your Battlesnake from colliding with others.
     const otherSnakes = gameState.board.snakes
-    for(var i = 0 ; i < otherSnakes.length; i++) {
+    for (var i = 0; i < otherSnakes.length; i++) {
         const otherSnake = otherSnakes[i]
 
-        if (otherSnake.body.some (coord => coord.x === targetUp.x && coord.y === targetUp.y)) {
+        if (otherSnake.body.some(coord => coord.x === targetUp.x && coord.y === targetUp.y)) {
             possibleMoves.up = false
         }
         if (otherSnake.body.some(coord => coord.x === targetDown.x && coord.y === targetDown.y)) {
-            possibleMoves.down = false 
-        }    
+            possibleMoves.down = false
+        }
         if (otherSnake.body.some(coord => coord.x === targetLeft.x && coord.y === targetLeft.y)) {
-            possibleMoves.left = false 
+            possibleMoves.left = false
         }
         if (otherSnake.body.some(coord => coord.x === targetRight.x && coord.y === targetRight.y)) {
-            possibleMoves.right = false 
+            possibleMoves.right = false
         }
-    }    
+    }
 
     // TODO: Step 4 - Find food.
     // Use information in gameState to seek out and find food.
     const allFood = gameState.board.food
-    
-    for(var i = 0; i < allFood.length; i++) {
+
+    for (var i = 0; i < allFood.length; i++) {
         const food = allFood
-
-        var arr = new Array(allFood); 
-        var sorted = arr.sort(); 
-        console.log(allFood.sort);
     }
-
-    
-    
-   
-    
-
-
-    // Finally, choose a move from the available safe moves.
-    // TODO: Step 5 - Select a move to make based on strategy, rather than random.
-    const safeMoves = Object.keys(possibleMoves).filter(key => possibleMoves[key])
+    const closestFood = {};
+    const safeMoves = Object.keys(possibleMoves).filter(key => possibleMoves[key]);
+    for (let candidateMove in safeMoves) {
+    const myNewHead = calculateNewHead(myHead, candidateMove);
+        let mySnakeToFood = distanceToClosestFood
+        mySnakeToFood 
+    }
     const response: MoveResponse = {
         move: safeMoves[Math.floor(Math.random() * safeMoves.length)],
     }
@@ -135,3 +128,54 @@ export function move(gameState: GameState): MoveResponse {
     console.log(`${gameState.game.id} MOVE ${gameState.turn}: ${response.move}`)
     return response
 }
+
+export function calculateNewHead(old: Coord, candidateMove: String): Coord {
+    if (candidateMove === "left") {
+        return {
+            x: old.x - 1,
+            y: old.y,
+        };
+    }
+    if (candidateMove === "right") {
+        return {
+            x: old.x + 1,
+            y: old.y,
+        };
+    }
+    if (candidateMove === "up") {
+        return {
+            x: old.x,
+            y: old.y + 1,
+        };
+    }
+    if (candidateMove === "down") {
+        return {
+            x: old.x,
+            y: old.y - 1,
+        };
+    }
+
+    return { x: 0, y: 0 };
+}
+export function distanceToFood(myHead: Coord, food: Coord): number {
+    return Math.abs(myHead.x - food.x) + Math.abs(myHead.y - food.y)
+}
+export function distanceToClosestFood(myHead: Coord, allFood: Coord[]): number {
+    let minimum = undefined;
+    for (var i = 0; i < allFood.length; i++) { 
+        const food = allFood[i];console.log(food)
+        let foodDistance = distanceToFood(myHead , food);
+        if (minimum === undefined || foodDistance < minimum) {
+            minimum = foodDistance;
+        }
+    }
+    if (minimum !== undefined) {
+        return minimum
+    } else {
+        return 1
+    }
+}
+
+    // Finally, choose a move from the available safe moves.
+    // TODO: Step 5 - Select a move to make based on strategy, rather than random
+
--- test/logic.test.ts
+++ test/logic.test.ts
@@ -1,11 +1,31 @@
-import { info, move } from '../src/logic'
+import { info, move, calculateNewHead, distanceToFood } from '../src/logic'
 import { Battlesnake, Coord, GameState, MoveResponse } from '../src/types';
 
 function createGameState(me: Battlesnake): GameState {
     return {
         game: {
             id: "",
-            ruleset: { name: "", version: "" },
+            ruleset: {
+                name: "",
+                version: "",
+                settings: {
+                   foodSpawnChance: 0.1,
+                   hazardDamagePerTurn: 1,
+                   hazardMap: "", 
+                   hazardMapAuthor: "",
+                   minimumFood: 1,
+                   royale: {
+                    shrinkEveryNTurns: 0,
+                   },
+                   squad: {
+                    allowBodyCollisions: false,
+                    sharedElimination: false,
+                    sharedHealth: false,
+                    sharedLength: false,
+                   },
+                },
+            },
+            source: "",
             timeout: 0
         },
         turn: 0,
@@ -30,9 +50,15 @@ function createBattlesnake(id: string, body: Coord[]): Battlesnake {
         head: body[0],
         length: body.length,
         shout: "",
-        squad: ""
+        squad: "",
+        customizations: {
+            color: "",
+            head: "",
+            tail: "",
+        },
     }
 }
+
 
 describe('Battlesnake API Version', () => {
     it('should be api version 1', () => {
@@ -42,6 +68,57 @@ describe('Battlesnake API Version', () => {
 })
 
 describe('Battlesnake Moves', () => {
+    it('it can calculate the distance between snake and food', () => {
+        let calculatedDistance = distanceToFood(
+            {x: 7, y: 8},
+            {x: 2, y: 1}
+        );
+        expect(calculatedDistance).toBe(12);
+
+        calculatedDistance = distanceToFood(
+            {x: 2, y: 1},
+            {x: 7, y: 8}
+        );
+        expect(calculatedDistance).toBe(12);
+
+    })
+    it('it can calculate the new head position', () => {
+        let calculatedResult = calculateNewHead(
+            {x: 4, y: 5},
+            "left"
+        );
+        expect(calculatedResult.y).toBe(5);
+        expect(calculatedResult.x).toBe(3);
+
+        calculatedResult = calculateNewHead(
+            {x: 13, y: 7},
+            "left"
+        );
+        expect(calculatedResult.y).toBe(7);
+        expect(calculatedResult.x).toBe(12);
+
+        calculatedResult = calculateNewHead(
+            {x: 4, y: 5},
+            "right"
+        );
+        expect(calculatedResult.y).toBe(5);
+        expect(calculatedResult.x).toBe(5);
+
+        calculatedResult = calculateNewHead(
+            {x: 4, y: 5},
+            "up"
+        );
+        expect(calculatedResult.y).toBe(6);
+        expect(calculatedResult.x).toBe(4);
+
+        calculatedResult = calculateNewHead(
+            {x: 4, y: 5},
+            "down"
+        );
+        expect(calculatedResult.y).toBe(4);
+        expect(calculatedResult.x).toBe(4);
+    })
+
     it('should never move into its own neck', () => {
         // Arrange
         const me = createBattlesnake("me", [{ x: 2, y: 0 }, { x: 1, y: 0 }, { x: 0, y: 0 }])
